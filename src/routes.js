a5c8c855c24b291e3b8d1e9ea936cca6957bb435
Yo Yehudi <yochannah@gmail.com>
2019-01-09 17:02:32 +0000

    allow CORS

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,14 @@
 var fetchIssues = require("./fetchIssues.js");
 var settings = require("../settings.js");
 var path = require("path");
+var cors = require("cors");
 
 
 //this is a VERY MINIMAL ROUTING APP, not designed to be a full server.
 var appRouter = function (app) {
 
   //API method to serve all issues fetched from GitHub.
-  app.get("/issues", function(req, res) {
+  app.get("/issues", cors(), function(req, res) {
     fetchIssues(settings).then(function(response){
       res.status(200).send(response);
     });
